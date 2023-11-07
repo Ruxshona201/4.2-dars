28e3c632b590881226bcdb1924c55f4fcd691060
unknown <ruxshonaabdupattoyeva2010@gmail.com>
2023-11-07 22:17:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,149 @@
+// 1.
+function a(x, y) {
+    console.log(x + y);
+}
+
+a(5, 10);
+a(2,3);
+
+
+// 2.
+function toSekunds(minutes) {
+   console.log( minutes*60)
+    }
+    toSekunds(5);
+
+    toSekunds(3);
+    toSekunds(2);
+
+// 3.
+function nextInteger(int) {
+    console.log(int+1);
+    }
+    nextInteger(0);
+    nextInteger(9);
+    nextInteger(-3);
+    
+// 4.
+function uchburchakYuzi(asos, balandlik) {
+    console.log(asos*balandlik/2);
+    }
+    uchburchakYuzi(3, 2);
+    uchburchakYuzi(7, 4);
+    uchburchakYuzi(10, 10);
+
+// 5.
+function ageToDays(age) {
+    console.log(age*365);
+    }
+    ageToDays(65);
+    ageToDays(0);
+    ageToDays(20);
+    
+// 6.
+function kub(son) {
+ console.log(son*son*son);
+}
+kub(3);
+kub(5);
+kub(10);
+
+// 7
+
+// 8.
+function power(x,y) {
+  console.log(x*y);
+    }
+    power(230, 10);
+    power(110, 3);
+    power(480, 20);
+
+    // 9.
+    function hourToSekunds(hour) {
+       console.log(hour*3600)
+        }
+    hourToSekunds(2);
+    hourToSekunds(10);
+    hourToSekunds(24);
+    
+    // 10.
+    function uchinchiTomon(tomon1, tomon2) {
+        console.log(tomon1+tomon2-1)
+    }
+    uchinchiTomon(8, 10);
+    uchinchiTomon(5, 7);
+    uchinchiTomon(9, 2);
+
+    // 11.
+    function qoldiq(son1, son2) {
+        console.log(son1/son2)
+    }
+    qoldiq(1, 3);
+    qoldiq(3, 4);
+    qoldiq(-9, 45);
+    qoldiq(5, 5);
+ 
+    // 12.
+    function turtburchakYuzi(boyi, eni) {
+        console.log(boyi*2+eni*2)
+        }
+        turtburchakYuzi(6, 7);
+        turtburchakYuzi(20, 10);
+        turtburchakYuzi(2, 9);
+    
+    // 13.
+    function stringQoshish (a) {
+        return a + " is better than nothing";
+    }
+    console.log(stringQoshish("something"));
+
+    // 14.
+    function kvadrat(a) {
+        return a ** 2
+    }
+    console.log(kvadrat(5));
+
+        // 15.
+        function sekundlar(soat, minut) {
+            console.log(soat*3600+minut*60)
+            }
+            sekundlar(1, 3);
+            sekundlar(2, 0);
+            sekundlar(0, 0);
+
+    // 16.
+function ichkiBurchaklar(n) {
+    return (n - 2) * 180
+}
+
+console.log(ichkiBurchaklar(4));
+
+    // 17.
+    function ochkolar(x, y) {
+        return (2 * x) + (3 * y)
+    }
+    console.log(ochkolar(7, 5));
+
+    // 18.
+function oxiridanQoshish(a) {
+    return "Mubashir" + a
+}
+console.log(oxiridanQoshish(" Edabit"));
+
+    // 19.
+    function ikkitaSon(x,y) {
+        if(x+y<100) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    console.log(ikkitaSon(5, 45));
+    
+    // 20.
+    function vaOperatori(a) {
+        return a > 0 && true
+    }
+    console.log(vaOperatori(5));
+    
